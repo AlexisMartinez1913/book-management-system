5774b9043d9a25255cfd9c2c8d195f93a5886995
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-21 11:11:22 -0500

    The entity Author is created

---
+++ BookManagementSystem/Models/Author.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookManagementSystem.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Nombre Completo")]
+        public string FullName { get; set; }
+
+        [Required]
+        [DisplayName("Fecha De Nacimiento")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [DisplayName("Ciudad De Procedencia")]
+        public string City { get; set; }
+
+        [Required]
+        [DisplayName("Correo Electronico")]
+        public string Email { get; set; }
+
+
+    }
+}
