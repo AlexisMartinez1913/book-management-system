9c763fcb1b5baf98dba8362a93a57efb7d3ad991
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-21 14:02:53 -0500

    The interface is created

---
+++ BookManagementSystem/Interfaces/IAuthorService.cs
@@ -0,0 +1,11 @@
+﻿using BookManagementSystem.Dtos;
+
+namespace BookManagementSystem.Interfaces
+{
+    public interface IAuthorService
+    {
+        Task<List<AuthorDTO>> GetAllAuthors();
+        Task<AuthorDTO> CreateAuthor(AuthorDTO authorDTO);
+        Task<bool> AuthorExists(int authorId);
+    }
+}
---
+++ BookManagementSystem/Interfaces/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using BookManagementSystem.Dtos;
+
+namespace BookManagementSystem.Interfaces
+{
+    public interface IBookService
+    {
+        Task<List<BookDTO>> GetAllBooks();
+        Task<BookDTO> CreateBook(BookDTO bookDTO);
+    }
+}
