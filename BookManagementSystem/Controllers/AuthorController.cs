16afc253e48088ae5e1ae5c7177fbfd6d86a7c7a
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-21 15:33:19 -0500

    Create BookController and AuthorController

---
+++ BookManagementSystem/Controllers/AuthorController.cs
@@ -0,0 +1,38 @@
+﻿using BookManagementSystem.Dtos;
+using BookManagementSystem.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementSystem.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IAuthorService _authorService;
+
+        public AuthorController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _authorService.GetAllAuthors();
+            return View(authors);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AuthorDTO authorDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(authorDto);
+            }
+
+            await _authorService.CreateAuthor(authorDto);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookManagementSystem/Controllers/BookController.cs
@@ -0,0 +1,57 @@
+﻿using BookManagementSystem.Dtos;
+using BookManagementSystem.Exceptions;
+using BookManagementSystem.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementSystem.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+        private readonly IAuthorService _authorService;
+
+        public BookController(IBookService bookService, IAuthorService authorService)
+        {
+            _bookService = bookService;
+            _authorService = authorService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookService.GetAllBooks();
+            return View(books);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.Authors = await _authorService.GetAllAuthors();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(BookDTO bookDto)
+        {
+            if (!ModelState.IsValid) 
+            {
+                ViewBag.Authors = await _authorService.GetAllAuthors();
+                return View(bookDto);
+            }
+
+            try
+            {
+                await _bookService.CreateBook(bookDto);
+                return RedirectToAction("Index");
+            }
+            catch (MaxBooksExceededException ex) 
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+            catch (AuthorNotFoundException ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+            }
+
+            ViewBag.Authors = await _authorService.GetAllAuthors();
+            return View(bookDto);
+        }
+    }
+}
