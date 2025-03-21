c2e6bbc7c06571858933044f205a79b6acd0a65f
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-21 12:18:15 -0500

    The entity Book is created

--- BookManagementSystem/Models/Author.cs
+++ BookManagementSystem/Models/Author.cs
@@ -8,21 +8,27 @@ namespace BookManagementSystem.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El Nombre Es Obligatorio")]
         [DisplayName("Nombre Completo")]
+        [MaxLength(255)]
         public string FullName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La Fecha De Nacimiento Es Obligatoria")]
         [DisplayName("Fecha De Nacimiento")]
         public DateTime BirthDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La Ciudad De Procedencia Es Obligatoria")]
         [DisplayName("Ciudad De Procedencia")]
+        [MaxLength(100)]
         public string City { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
         [DisplayName("Correo Electrónico")]
+        [EmailAddress(ErrorMessage = "Correo Inválido")]
+        [MaxLength(255)]
         public string Email { get; set; }
+
+        public List<Book> Books { get; set; } = new List<Book>();
 
 
     }
---
+++ BookManagementSystem/Models/Book.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookManagementSystem.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El titulo es requerido")]
+        [DisplayName("Titulo")]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "El año es obligatorio")]
+        [DisplayName("Año")]
+        [Range(0, 2025, ErrorMessage = "Año inválido")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "El Género es obligatorio")]
+        [DisplayName("Género")]
+        [MaxLength(100)]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "El número de páginas es obligatorio")]
+        [DisplayName("Número De Páginas")]
+        [Range(1, int.MaxValue, ErrorMessage = "El Número de páginas debe ser mayor a 0")]
+        public int PageCount { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId {  get; set; }
+        public Author Author { get; set; }
+
+
+    }
+}
