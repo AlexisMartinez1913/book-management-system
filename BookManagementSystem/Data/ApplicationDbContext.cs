b4713cf26a643aef1d7030ab202fb2b93a270b24
Alexis Martinez <jhonyalexis100@gmail.com>
2025-03-21 12:32:10 -0500

    The connection string is created

--- BookManagementSystem/Data/ApplicationDbContext.cs
+++ BookManagementSystem/Data/ApplicationDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace BookManagementSystem.Data
+﻿using BookManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagementSystem.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //Crear Tablas de la base de datos
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
     }
 }
--- BookManagementSystem/Program.cs
+++ BookManagementSystem/Program.cs
@@ -1,4 +1,11 @@
+using BookManagementSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//AGREGAR CADENA DE CONEXION
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
